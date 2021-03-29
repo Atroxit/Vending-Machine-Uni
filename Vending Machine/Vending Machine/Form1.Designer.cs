ab9982159f2f4b38eaa2f48c2aaceb63df38ee18
James <jamesalison25@gmail.com>
2021-03-29 05:53:51 +0100

    Finished Cash Section

--- Vending Machine/Vending Machine/Form1.Designer.cs
+++ Vending Machine/Vending Machine/Form1.Designer.cs
@@ -26,8 +26,8 @@ namespace Vending_Machine
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -44,7 +44,7 @@ namespace Vending_Machine
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.DrinkSelectionInput = new System.Windows.Forms.TextBox();
             this.BuyButton = new System.Windows.Forms.Button();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
@@ -54,22 +54,18 @@ namespace Vending_Machine
             // CardIDDropdown
             // 
             this.CardIDDropdown.FormattingEnabled = true;
-            this.CardIDDropdown.Items.AddRange(new object[] {
-            "Card ID 1",
-            "Card ID 2",
-            "Card ID 3",
-            "Card ID 4"});
-            this.CardIDDropdown.Location = new System.Drawing.Point(470, 14);
+            this.CardIDDropdown.Items.AddRange(new object[] {"Card ID 1", "Card ID 2", "Card ID 3", "Card ID 4"});
+            this.CardIDDropdown.Location = new System.Drawing.Point(403, 12);
             this.CardIDDropdown.Name = "CardIDDropdown";
-            this.CardIDDropdown.Size = new System.Drawing.Size(121, 23);
+            this.CardIDDropdown.Size = new System.Drawing.Size(104, 21);
             this.CardIDDropdown.TabIndex = 1;
             this.CardIDDropdown.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(406, 16);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.Location = new System.Drawing.Point(348, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 21);
             this.label1.TabIndex = 7;
@@ -78,43 +74,43 @@ namespace Vending_Machine
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 99);
+            this.label2.Location = new System.Drawing.Point(27, 86);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 45);
+            this.label2.Size = new System.Drawing.Size(34, 39);
             this.label2.TabIndex = 8;
             this.label2.Text = "Pipsi\r\n£2.00\r\nA1";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(111, 99);
+            this.label3.Location = new System.Drawing.Point(95, 86);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 45);
+            this.label3.Size = new System.Drawing.Size(34, 39);
             this.label3.TabIndex = 9;
             this.label3.Text = "Cola\r\n£1.70\r\nA2";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(190, 99);
+            this.label4.Location = new System.Drawing.Point(163, 86);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 45);
+            this.label4.Size = new System.Drawing.Size(51, 39);
             this.label4.TabIndex = 10;
             this.label4.Text = "Diet Pipsi\r\n£2.20\r\nA3";
             // 
             // CashInput
             // 
-            this.CashInput.Location = new System.Drawing.Point(295, 14);
+            this.CashInput.Location = new System.Drawing.Point(253, 12);
             this.CashInput.Name = "CashInput";
-            this.CashInput.Size = new System.Drawing.Size(100, 23);
+            this.CashInput.Size = new System.Drawing.Size(86, 20);
             this.CashInput.TabIndex = 14;
             this.CashInput.TextChanged += new System.EventHandler(this.CashInput_TextChanged);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(238, 16);
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label8.Location = new System.Drawing.Point(204, 14);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(44, 21);
             this.label8.TabIndex = 15;
@@ -123,89 +119,89 @@ namespace Vending_Machine
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(190, 181);
+            this.label5.Location = new System.Drawing.Point(163, 157);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(73, 45);
+            this.label5.Size = new System.Drawing.Size(67, 39);
             this.label5.TabIndex = 18;
             this.label5.Text = "Diet Dr. Cola\r\n£1.70\r\nB3";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(111, 181);
+            this.label6.Location = new System.Drawing.Point(95, 157);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(55, 45);
+            this.label6.Size = new System.Drawing.Size(50, 39);
             this.label6.TabIndex = 17;
             this.label6.Text = "Diet Cola\r\n£2.00\r\nB2";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(32, 181);
+            this.label7.Location = new System.Drawing.Point(27, 157);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(49, 45);
+            this.label7.Size = new System.Drawing.Size(45, 39);
             this.label7.TabIndex = 16;
             this.label7.Text = "Dr. Cola\r\n£1.50\r\nB1";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(190, 259);
+            this.label9.Location = new System.Drawing.Point(163, 224);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(64, 45);
+            this.label9.Size = new System.Drawing.Size(59, 39);
             this.label9.TabIndex = 21;
             this.label9.Text = "Diet Power\r\n£3.40\r\nC3";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(111, 259);
+            this.label10.Location = new System.Drawing.Point(95, 224);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(66, 45);
+            this.label10.Size = new System.Drawing.Size(60, 39);
             this.label10.TabIndex = 20;
             this.label10.Text = "Max Power\r\n£3.00\r\nC2";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(32, 259);
+            this.label11.Location = new System.Drawing.Point(27, 224);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(72, 45);
+            this.label11.Size = new System.Drawing.Size(66, 39);
             this.label11.TabIndex = 19;
             this.label11.Text = "Power Thirst\r\n£3.40\r\nC1";
             // 
-            // textBox2
+            // DrinkSelectionInput
             // 
-            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.textBox2.Location = new System.Drawing.Point(404, 181);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 22;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.DrinkSelectionInput.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.DrinkSelectionInput.Location = new System.Drawing.Point(346, 157);
+            this.DrinkSelectionInput.Name = "DrinkSelectionInput";
+            this.DrinkSelectionInput.Size = new System.Drawing.Size(86, 20);
+            this.DrinkSelectionInput.TabIndex = 22;
             // 
             // BuyButton
             // 
-            this.BuyButton.Location = new System.Drawing.Point(417, 229);
+            this.BuyButton.Location = new System.Drawing.Point(357, 198);
             this.BuyButton.Name = "BuyButton";
-            this.BuyButton.Size = new System.Drawing.Size(75, 23);
+            this.BuyButton.Size = new System.Drawing.Size(64, 20);
             this.BuyButton.TabIndex = 23;
             this.BuyButton.Text = "Buy";
             this.BuyButton.UseVisualStyleBackColor = true;
+            this.BuyButton.Click += new System.EventHandler(this.BuyButton_Click);
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(415, 147);
+            this.label12.Location = new System.Drawing.Point(356, 127);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(79, 15);
+            this.label12.Size = new System.Drawing.Size(74, 13);
             this.label12.TabIndex = 24;
             this.label12.Text = "Enter Drink ID";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label13.Location = new System.Drawing.Point(238, 48);
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label13.Location = new System.Drawing.Point(204, 42);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(66, 21);
             this.label13.TabIndex = 25;
@@ -214,8 +210,8 @@ namespace Vending_Machine
             // ChangeLabel
             // 
             this.ChangeLabel.AutoSize = true;
-            this.ChangeLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ChangeLabel.Location = new System.Drawing.Point(318, 48);
+            this.ChangeLabel.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ChangeLabel.Location = new System.Drawing.Point(273, 42);
             this.ChangeLabel.Name = "ChangeLabel";
             this.ChangeLabel.Size = new System.Drawing.Size(49, 21);
             this.ChangeLabel.TabIndex = 26;
@@ -223,14 +219,14 @@ namespace Vending_Machine
             // 
             // VendingMachine
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 344);
+            this.ClientSize = new System.Drawing.Size(517, 298);
             this.Controls.Add(this.ChangeLabel);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.BuyButton);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.DrinkSelectionInput);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label11);
@@ -248,7 +244,6 @@ namespace Vending_Machine
             this.Text = "Vending Machine";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
 
@@ -266,7 +261,7 @@ namespace Vending_Machine
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox DrinkSelectionInput;
         private System.Windows.Forms.Button BuyButton;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
--- Vending Machine/Vending Machine/Form1.cs
+++ Vending Machine/Vending Machine/Form1.cs
@@ -12,34 +12,88 @@ namespace Vending_Machine
 {
     public partial class VendingMachine : Form
     {
-        public float balance = 0.00f;
+        private float balance = 0.00f;
+
+        //Key = ID, Value = Price
+        Dictionary<string, float> drinks = new Dictionary<string, float>();
+
         public VendingMachine()
         {
             InitializeComponent();
+
+            //First Row
+            drinks.Add("A1", 2.00f); //Pipsi
+            drinks.Add("A2", 1.70f); //Cola
+            drinks.Add("A3", 2.20f); //Diet Pipsi
+
+            //Second Row
+            drinks.Add("B1", 1.50f); //Dr. Cola
+            drinks.Add("B2", 2.00f); //Diet Cola
+            drinks.Add("B3", 1.70f); //Diet Dr. Cola
+
+            //Third Row
+            drinks.Add("C1", 3.40f); //Power Thirst
+            drinks.Add("C2", 3.00f); //Max Power
+            drinks.Add("C3", 3.40f); //Diet Power
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             var x = CardIDDropdown.SelectedIndex;
-
+            Console.WriteLine(x);
         }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void CashInput_TextChanged(object sender, EventArgs e)
         {
+            if(CashInput.Text.Equals("")) return;
             try
             {
-                balance = float.Parse(CashInput.Text);
+                this.balance = float.Parse(CashInput.Text);
             }
             catch (Exception)
             {
                 Console.WriteLine("Error Parsing Cash Input");
             }
-            
+
+        }
+
+        private void BuyButton_Click(object sender, EventArgs e)
+        {
+            float price = getPrice(DrinkSelectionInput.Text);
+            Console.WriteLine(price);
+            if (price == 0)
+            {
+                MessageBox.Show("You have entered an Invalid ID", "Invalid ID");
+                return;
+            }
+
+            if (this.balance >= price)
+            {
+                this.balance -= price;
+                ChangeLabel.Text = "£" + this.balance.ToString();
+                resetMachine();
+            }
+            else
+            {
+                MessageBox.Show("You can't afford this item. Please select another item.", "Insufficient Funds");
+            }
+
+        }
+
+        private void resetMachine()
+        {
+            this.balance = 0;
+            CashInput.Text = "";
+            DrinkSelectionInput.Text = "";
+        }
+
+        private float getPrice(String ID)
+        {
+            float value;
+            drinks.TryGetValue(ID, out value);
+
+            return value;
         }
     }
 }
